0dbc44dc68291447e9fe3998d5812c5d9909e188
MitchBath <mbath@uncc.edu>
2020-04-29 08:44:28 +0000

    Add comments controller

---
+++ ginder/app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+    def create
+        @profile = Profile.find(params[:profile_id])
+        @comment = @profile.comments.create(comment_params)
+        redirect_to profile_path(@profile)
+    end
+    
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
+end
