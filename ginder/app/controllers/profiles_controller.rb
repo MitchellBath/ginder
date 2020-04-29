7afa1024b8edb7c8869a8e592273c96d71aa8753
MitchBath <mbath@uncc.edu>
2020-04-29 08:07:30 +0000

    Completed new profile creation

--- ginder/app/controllers/profiles_controller.rb
+++ ginder/app/controllers/profiles_controller.rb
@@ -3,6 +3,17 @@ class ProfilesController < ApplicationController
     end
     
     def create
+        @profile = Profile.new(profile_params)
+        @profile.save
         render plain: params[:profile].inspect
     end
+    
+    def show
+        @profile = Profile.find(params[:id])
+    end
+    
+    private
+        def profile_params
+            params.require(:profile).permit(:name, :biography, :favorite_game, :casual_or_competitive)
+        end
 end
