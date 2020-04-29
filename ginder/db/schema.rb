a28576d8b5a6371d1e6d332a2d6938484b492304
MitchBath <mbath@uncc.edu>
2020-04-29 09:52:56 +0000

    Fixed db migration error with users

--- ginder/db/schema.rb
+++ ginder/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200429084026) do
+ActiveRecord::Schema.define(version: 20200429094214) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20200429084026) do
     t.string   "casual_or_competitive"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
