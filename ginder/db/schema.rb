8b0e731e6bedd6b42e31669adf1b82cbe748a586
MitchBath <mbath@uncc.edu>
2020-04-29 21:01:17 +0000

    Changed background color and added user_id to profiles in a migration. Broken commit

--- ginder/db/schema.rb
+++ ginder/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200429094214) do
+ActiveRecord::Schema.define(version: 20200429205723) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
