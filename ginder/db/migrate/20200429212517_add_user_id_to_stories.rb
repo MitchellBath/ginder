b7893f60ec56866e93825f45e32fd8f4601305eb
MitchBath <mbath@uncc.edu>
2020-04-29 21:31:32 +0000

    Fixed major bug where users could not create profiles. Added user_id to SQL tables using migration

---
+++ ginder/db/migrate/20200429212517_add_user_id_to_stories.rb
@@ -0,0 +1,4 @@
+class AddUserIdToStories < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- ginder/db/schema.rb
+++ ginder/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200429205723) do
+ActiveRecord::Schema.define(version: 20200429212941) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20200429205723) do
     t.string   "casual_or_competitive"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
